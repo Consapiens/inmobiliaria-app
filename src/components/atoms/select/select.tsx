d76296001195eebcd844700d60cdefe0a214efa6
alejandro <alejandro2397gil@gmail.com>
2021-12-07 14:08:04 -0500

    Correcciones

--- src/components/atoms/select/select.tsx
+++ src/components/atoms/select/select.tsx
@@ -2,25 +2,44 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import React from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import { selectItem } from '../../../shared/models/select.model';
+import FormControl from '@mui/material/FormControl';
+import { styled } from '@mui/material/styles';
 
 interface Props {
   items: selectItem[];
   label: string;
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (value: string) => void;
 }
+
+const CustomStyles = styled(Select)(() => ({
+  backgroundColor: '#fff',
+}));
 
 const _Select: React.FC<Props> = (props) => {
   const handleChange = (event: SelectChangeEvent) => {
-    props.value = event.target.value;
+    props.onChange(event.target.value);
   };
 
   return (
-    <Select label={props.label} value={props.value} onChange={handleChange}>
-      {props.items.map((item, i) => {
-        return <MenuItem value={item.value}>{item.description}</MenuItem>;
-      })}
-    </Select>
+    <>
+      <FormControl sx={{ minWidth: 200 }}>
+        <Select
+          label={props.label}
+          value={props.value}
+          onChange={handleChange}
+          displayEmpty
+          input={<CustomStyles />}
+        >
+          <MenuItem value="">
+            <em>{props.label}</em>
+          </MenuItem>
+          {props.items.map((item, i) => {
+            return <MenuItem value={item.value}>{item.description}</MenuItem>;
+          })}
+        </Select>
+      </FormControl>
+    </>
   );
 };
 
--- src/components/organisms/banner/banner.tsx
+++ src/components/organisms/banner/banner.tsx
@@ -2,37 +2,49 @@ import React, { Component, useState } from 'react';
 import { selectItem } from '../../../shared/models/select.model';
 import _Button from '../../atoms/button/button';
 import _Select from '../../atoms/select/select';
+import './banner.scss';
 
 export default function Banner() {
   const [tipoInmueble, setTipoInmueble] = useState('');
-
-  const options = [
-    { value: 'Tipo de inmueble', label: 'Tipo de inmueble' },
-    { value: 'Proyectos Nuevos', label: 'Proyectos Nuevos' },
-    { value: 'Ubicación ', label: 'Ubicación' },
-  ];
+  const [caracteristicaInmueble, setCaracteristicaInmueble] = useState('');
 
   const tiposInmueble: selectItem[] = [
     { value: 1, description: 'Apartamento' },
     { value: 2, description: 'Casa' },
   ];
 
-  const changeTipoInmueble = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTipoInmueble(event.target.value);
+  const caracteristicasInmueble: selectItem[] = [
+    { value: 1, description: 'Nuevas' },
+    { value: 2, description: 'Usadas' },
+  ];
+
+  const changeTipoInmueble = (value: string) => {
+    setTipoInmueble(value);
+  };
+  const changeCaracteristicaInmueble = (value: string) => {
+    setCaracteristicaInmueble(value);
   };
 
   return (
     <div className="banner">
-      <h2>Encuentra inmuebles en venta y arriendo:</h2>
-      <_Select
-        label="Tipo de inmueble"
-        items={tiposInmueble}
-        value={tipoInmueble}
-        onChange={}
-      />
-      {/* <_Select />
-      <_Select /> */}
-      <_Button />
+      <div className="banner__wrap">
+        <h2>Encuentra inmuebles en venta y arriendo:</h2>
+        <div className="banner__form">
+          <_Select
+            label="Tipo de inmueble"
+            items={tiposInmueble}
+            value={tipoInmueble}
+            onChange={changeTipoInmueble}
+          />
+          <_Select
+            label="Estado"
+            items={caracteristicasInmueble}
+            value={caracteristicaInmueble}
+            onChange={changeCaracteristicaInmueble}
+          />
+          <_Button />
+        </div>
+      </div>
     </div>
   );
 }
